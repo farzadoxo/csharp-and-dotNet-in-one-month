4449e202fb1c2120b89f8d7dafc49acd01b933c2
farzadoxo <farzadebrahimi.oxo@gmail.com>
2025-02-12 07:43:32 -0800

    register DTO added

--- WebApp/DTOs/UserDTO.cs
+++ WebApp/DTOs/UserDTO.cs
@@ -12,4 +12,10 @@ namespace MyWebAPI.DTOs
         public int Count { get; set; }
         public ICollection<User> Users { get; set;}
     }
+
+    public class RegisterDTO
+    {
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+    }
 }
