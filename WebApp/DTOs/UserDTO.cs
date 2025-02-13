976906c283174dc6052951212bf699bf27e736fe
farzadoxo <farzadebrahimi.oxo@gmail.com>
2025-02-13 10:39:54 -0800

    user dto updated

--- WebApp/DTOs/UserDTO.cs
+++ WebApp/DTOs/UserDTO.cs
@@ -13,9 +13,5 @@ namespace MyWebAPI.DTOs
         public ICollection<User> Users { get; set;}
     }
 
-    public class RegisterDTO
-    {
-        public string? UserName { get; set; }
-        public string? Password { get; set; }
-    }
+    
 }
