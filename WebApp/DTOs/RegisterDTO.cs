90ce903307ace56fcacbc44f64a4b1465a0cde9d
farzadoxo <farzadebrahimi.oxo@gmail.com>
2025-02-18 22:10:55 +0330

    Contractor defined

--- WebApp/DTOs/RegisterDTO.cs
+++ WebApp/DTOs/RegisterDTO.cs
@@ -1,5 +1,10 @@
 public class RegisterDTO
     {
-        public string? UserName { get; set; }
-        public string? Password { get; set; }
+        public RegisterDTO(string username,string password)
+        {
+            this.UserName = username;
+            this.Password = password;
+        }
+        public string UserName { get; set; }
+        public string Password { get; set; }
     }
