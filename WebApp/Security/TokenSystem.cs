15a29e4d7cdea472aa0c8e5859d5e793274e06b1
farzadoxo <farzadebrahimi.oxo@gmail.com>
2025-02-20 10:57:09 +0330

    system token added

---
+++ WebApp/Security/TokenSystem.cs
@@ -0,0 +1,46 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+
+public class TokenSystem
+    {
+        public static string GenerateEncodedToken(IdentityUser user)
+        {
+            var claims = new[]
+            {
+                new Claim("UserId", user.Id.ToString()),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("behnam be hazrat @34 abas peydat konam *"));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: "www.mft.com",
+                audience: "www.mft.com",
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(2000),
+                signingCredentials: creds);
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public static ClaimsPrincipal Validate(string token)
+        {
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            TokenValidationParameters parameters = new TokenValidationParameters()
+            {
+                RequireExpirationTime = true,
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("behnam be hazrat @34 abas peydat konam *"))
+            };
+
+            ClaimsPrincipal principal = handler.ValidateToken(token,parameters,out SecurityToken securityToken);
+            return principal;
+        }
+    }
+
+
+
